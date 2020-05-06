a4a1ac74cf36b733c5ea158a07dcf984841c5695
egor <ivashin.egor98@gmail.com>
2020-05-06 17:16:46 +0300

    насследование

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-class Worker{
-  constructor(firstName, lastName, rate, days){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.rate = rate;
-    this.days = days;
-  }
-  getName(){
-    return this.firstName
-  }
-  getSurname(){
-    return this.lastName
-  }
-  getRate(){
-    return this.rate
-  }
-  getDays(){
-    return this.days
-  }
-  getSalary(){
-    return this.rate * this.days
+class User {
+  constructor(options){
+    this.firstName = options.firstName;
+    this.lastName = options.lastName;
+    this.age = options.age;
+    this.gender = options.gender;
+    this.phone = options.phone;
   }
 }
-let worker = new Worker('Иван', 'Иванов', 10, 31);
-console.log(worker.getName());
-console.log(worker.getSurname());
-console.log(worker.getRate());
-console.log(worker.getDays());
-console.log(worker.getSalary());
+
+class Developer extends User{
+  constructor(options){
+    super(options)
+    this.firstName = options.firstName;
+    this.lastName = options.lastName;
+    this.age = options.age;
+    this.gender = options.gender;
+    this.phone = options.phone;
+    this.skills = options.skills;
+    this.company = options.company;
+    this.tasks = options.tasks;
+  }
+  // метод, который добавляет скиллы пользователя
+  getskills(skills){
+    return this.skills = `${this.skills} ${skills}`
+  }
+
+  // сеттер, который перезаписывает номер телефона
+  set newPhon(newPhone){
+    this.phone = newPhone
+  }
+}
+
+class Designer extends User{
+  constructor(options){
+    super(options)
+    this.firstName = options.firstName;
+    this.lastName = options.lastName;
+    this.age = options.age;
+    this.gender = options.gender;
+    this.phone = options.phone;
+    this.skills = options.skills;
+    this.company = options.company;
+    this.tasks = options.tasks;
+  }
+}
+
+const user = new User({
+  firstName: "Ivan",
+  lastName: "Ivanov",
+  age: 21,
+  gender: "male",
+  phone: 999.999
+})
+
+const developer = new Developer({
+  firstName: "Vika",
+  lastName: "Ulitochka",
+  age: 29,
+  gender: "female",
+  phone: 95644545445,
+  skills: "HTML, CSS",
+  company: "DataArt",
+  tasks: "Завершить верстку"
+})
+
+const designer = new Designer({
+  firstName: "Vasya",
+  lastName: "Lastochka",
+  age: 33,
+  gender: "male",
+  phone: 95644545445,
+  skills: "Photoshop",
+  company: "Epam",
+  tasks: "Выполнить Web-дизайн"
+})
+
+console.log(user)
+developer.getskills("Java, Git")
+console.log(developer);
+console.log(designer)
