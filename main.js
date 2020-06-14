0d36e1448b626dc480ae801e7ec4106beb720ff7
Your Name <you@example.com>
2020-06-14 20:30:58 +0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// codesandbox https://codesandbox.io/s/json-server-whu3w?file=/db.json
+// JSON Server https://whu3w.sse.codesandbox.io/todos
+
 const todoList = document.querySelector(".todo-list");
 // const newTodo = document.querySelector('.new-todo')
-// const getRandomBoolean = () => Math.random() >= 0.5;
+const getRandomBoolean = () => Math.random() >= 0.5;
 const url = "https://whu3w.sse.codesandbox.io/todos";
 const todoCount = null;
 
@@ -14,9 +17,9 @@ const renderTodos = (contentText) => {
   for (let key in contentText) {
     const check = contentText[key].completed ? "checked" : "";
     const complet = contentText[key].completed ? "completed" : "";
-		console.log(contentText[key]);
-		const id = contentText[key].id;
-		
+    console.log(contentText[key]);
+    const id = contentText[key].id;
+
     todoList.innerHTML += `<li class="${complet}" id="${id}">
         <div class="view">
           <input class="toggle" type="checkbox" ${check} /><label
@@ -29,25 +32,91 @@ const renderTodos = (contentText) => {
 async function main() {
   const items = await getItems();
   renderTodos(items);
+  const newTodo = document.querySelector(".new-todo");
+  const filterActive = document.querySelector('[href="/active"]');
+  const filterAll = document.querySelector('[href="/"]');
+  const filterCompleted = document.querySelector('[href="/completed"]');
   const destroy = document.querySelectorAll(".destroy");
-  destroy.forEach((item) => {
-    item.addEventListener("click", deleteTodo);
+
+  newTodo.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13 && newTodo.value !== "") {
+      const todo = createTodo(newTodo.value);
+      todoList.append(todo);
+      newTodo.value = "";
+    }
+  });
+
+  filterAll.addEventListener("click", (event) => {
+    event.preventDefault();
+    allTodos();
+  });
+  filterActive.addEventListener("click", (event) => {
+    event.preventDefault();
+    activeTodos();
+  });
+  filterCompleted.addEventListener("click", (event) => {
+    event.preventDefault();
+    completedTodos();
   });
 }
 main();
 
-function deleteTodo() {
-	const todo = closest("li");
-	console.log(todo);
-	
-	todo.remove();
-	deleteItem()
+function createTodo(title) {
+  const todo = document.createElement("li");
+  todo.innerHTML = `<div class="view">
+  <!-- completed, editing -->
+  <input class="toggle" type="checkbox"/>
+  <!-- checked -->
+  <label>${title}</label>
+  <button class="destroy"></button>
+  </div>
+  <input type="text" class="edit" value="" />`;
+  const body = {
+    title,
+    completed: getRandomBoolean(),
+  };
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+  return todo;
 }
 
-function deleteItem(id) {
-  fetch(this.url + "/" + id, {
-    method: "DELETE",
-  })
-    .then(res => console.log(res.status))
-    .catch((error) => console.log(error));
+function allTodos() {
+  const todos = [...document.querySelectorAll(".todo-list li")];
+  todos.forEach((todo) => {
+    todo.style.display = "block";
+  });
+  fetch("https://whu3w.sse.codesandbox.io/todos")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  renderTodos();
+}
+
+function activeTodos() {
+  const todos = [...document.querySelectorAll(".todo-list li")];
+  todos.forEach((todo) => {
+    if (todo.classList.contains("completed")) {
+      todo.style.display = "none";
+    } else todo.style.display = "block";
+  });
+  fetch("https://whu3w.sse.codesandbox.io/todos?completed=false")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  renderTodos();
+}
+
+function completedTodos() {
+  const todos = [...document.querySelectorAll(".todo-list li")];
+  todos.forEach((todo) => {
+    if (!todo.classList.contains("completed")) {
+      todo.style.display = "none";
+    } else todo.style.display = "block";
+  });
 }
